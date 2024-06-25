b7c3413446771ebb1a1b63c2b45cee865e682497
Ajit Thapa <ajitthapa248@gmail.com>
2024-06-25 16:25:09 +0545

    (fix): Fixed CORS error

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
