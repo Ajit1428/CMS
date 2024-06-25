3d2decfd160d45268fcc2bcfc30a34031a4b68af
Ajit Thapa <ajitthapa248@gmail.com>
2024-06-25 16:28:07 +0545

    (fix): Updated the nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
